bd81ac69da756f3d2d90ba8a0556a9aed8f6ec9f
Óscar Santana Ortega <g0sk.dev@proton.me>
2022-12-26 16:22:51 +0000

    babel-plugin-module-resolver

--- src/screens/onboarding/SlideItem.tsx
+++ src/screens/onboarding/SlideItem.tsx
@@ -1,5 +1,6 @@
 import Button from '../../components/Button';
-import {View, Text} from 'react-native';
+import View from 'components/View';
+import Text from 'components/Text';
 import {type SlideProps} from './index';
 import {Image, useWindowDimensions, StyleSheet} from 'react-native';
 
@@ -12,10 +13,13 @@ const SlideItem = ({slide}: SlideItemProps) => {
 	return (
 		<View style={{flex: 1, width, justifyContent: 'center', alignItems: 'center'}}>
 			<View>
-				<Image source={slide.image} style={{height: 300, width: 300, resizeMode: 'contain'}} />
+				<Image
+					source={slide.image}
+					style={{width: width - 50, height: 300, resizeMode: 'contain'}}
+				/>
 			</View>
 			<View style={{flex: 0.3}}>
-				<Text>{slide.title}</Text>
+				<Text variant="title">{slide.title}</Text>
 			</View>
 			<Button label="Next" labelVariant="title" onPress={() => null} />
 		</View>
